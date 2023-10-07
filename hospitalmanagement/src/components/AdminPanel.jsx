96d3fba78e3997b703e5622e229ecefbe9af5137
Jestin Sabu <55315353+Jestin-Sabu@users.noreply.github.com>
2023-10-07 17:18:19 +0530

    Dashboard

--- hospitalmanagement/src/components/AdminPanel.jsx
+++ hospitalmanagement/src/components/AdminPanel.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import Nav from 'react-bootstrap/Nav';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Navbar from 'react-bootstrap/Navbar';
-import Dashboard from './PatientManagement/Dashboard';
+import Dashboard from './Dashboard/Dashboard';
 import PatientRecords from './PatientManagement/PatientRecords';
 import PatientRegistration from './PatientManagement/PatientRegistration';
 
@@ -18,50 +18,50 @@ const AdminPanel = () => {
 
     return (
         <div className="d-flex flex-row">
-                <Navbar className="bg-body-tertiary p-2 align-items-start" data-bs-theme="dark" style={{ width: "250px", height:"100vh" }} >
-                    <Nav className="flex-column" activeKey={activeKey} onSelect={handleSelect}>
-                        <Nav.Link eventKey="1">Dashboard</Nav.Link>
-                        <NavDropdown title="Patient Management" active={activeKey.split('.')[0] === "2"}>
-                            <NavDropdown.Item eventKey="2.1">Patient Registration</NavDropdown.Item>
-                            <NavDropdown.Item eventKey="2.2">Patient Records</NavDropdown.Item>
-                            <NavDropdown.Item eventKey="2.3">Admissions & Discharges</NavDropdown.Item>
-                            <NavDropdown.Item eventKey="2.4">Health Records</NavDropdown.Item>
-                        </NavDropdown>
-                        <NavDropdown title="Appointment Management" active={activeKey.split('.')[0] === "3"}>
-                            <NavDropdown.Item eventKey="3.1">Appointment Scheduling</NavDropdown.Item>
-                            <NavDropdown.Item eventKey="3.2">Calender View</NavDropdown.Item>
-                        </NavDropdown>
-                        <NavDropdown title="Staff Management" active={activeKey.split('.')[0] === "4"}>
-                            <NavDropdown.Item eventKey="4.1">User Administration</NavDropdown.Item>
-                            <NavDropdown.Item eventKey="4.2">Roles and Permission</NavDropdown.Item>
-                        </NavDropdown>
-                        <NavDropdown title="Inventory Management" active={activeKey.split('.')[0] === "5"}>
-                            <NavDropdown.Item eventKey="5.1">Medication & Supplies</NavDropdown.Item>
-                            <NavDropdown.Item eventKey="5.2">Order & Reorder</NavDropdown.Item>
-                        </NavDropdown>
-                        <NavDropdown title="Billing & Invoices" active={activeKey.split('.')[0] === "6"}>
-                            <NavDropdown.Item eventKey="6.1">Generate Invoices</NavDropdown.Item>
-                            <NavDropdown.Item eventKey="6.2">Payment Processing</NavDropdown.Item>
-                        </NavDropdown>
-                        <NavDropdown title="Reports & Analytics" active={activeKey.split('.')[0] === "7"}>
-                            <NavDropdown.Item eventKey="7.1">Generate Reports</NavDropdown.Item>
-                            <NavDropdown.Item eventKey="7.2">Data Visualization</NavDropdown.Item>
-                        </NavDropdown>
-                        <NavDropdown title="Security & Access Control" active={activeKey.split('.')[0] === "8"}>
-                            <NavDropdown.Item eventKey="8.1">User Authentication</NavDropdown.Item>
-                            <NavDropdown.Item eventKey="8.2">Audit Trails</NavDropdown.Item>
-                        </NavDropdown>
-                        <NavDropdown title="Communication" active={activeKey.split('.')[0] === "9"}>
-                            <NavDropdown.Item eventKey="9.1">Messaging</NavDropdown.Item>
-                            <NavDropdown.Item eventKey="9.2">Notifications</NavDropdown.Item>
-                        </NavDropdown>
-                    </Nav>
-                </Navbar>
+            <Navbar className="bg-body-tertiary p-2 align-items-start" data-bs-theme="dark" style={{ width: "250px", height: "100vh" }} >
+                <Nav className="flex-column" activeKey={activeKey} onSelect={handleSelect}>
+                    <Nav.Link eventKey="1">Dashboard</Nav.Link>
+                    <NavDropdown title="Patient Management" active={activeKey.split('.')[0] === "2"}>
+                        <NavDropdown.Item eventKey="2.1">Patient Registration</NavDropdown.Item>
+                        <NavDropdown.Item eventKey="2.2">Patient Records</NavDropdown.Item>
+                        <NavDropdown.Item eventKey="2.3">Admissions & Discharges</NavDropdown.Item>
+                        <NavDropdown.Item eventKey="2.4">Health Records</NavDropdown.Item>
+                    </NavDropdown>
+                    <NavDropdown title="Appointment Management" active={activeKey.split('.')[0] === "3"}>
+                        <NavDropdown.Item eventKey="3.1">Appointment Scheduling</NavDropdown.Item>
+                        <NavDropdown.Item eventKey="3.2">Calender View</NavDropdown.Item>
+                    </NavDropdown>
+                    <NavDropdown title="Staff Management" active={activeKey.split('.')[0] === "4"}>
+                        <NavDropdown.Item eventKey="4.1">User Administration</NavDropdown.Item>
+                        <NavDropdown.Item eventKey="4.2">Roles and Permission</NavDropdown.Item>
+                    </NavDropdown>
+                    <NavDropdown title="Inventory Management" active={activeKey.split('.')[0] === "5"}>
+                        <NavDropdown.Item eventKey="5.1">Medication & Supplies</NavDropdown.Item>
+                        <NavDropdown.Item eventKey="5.2">Order & Reorder</NavDropdown.Item>
+                    </NavDropdown>
+                    <NavDropdown title="Billing & Invoices" active={activeKey.split('.')[0] === "6"}>
+                        <NavDropdown.Item eventKey="6.1">Generate Invoices</NavDropdown.Item>
+                        <NavDropdown.Item eventKey="6.2">Payment Processing</NavDropdown.Item>
+                    </NavDropdown>
+                    <NavDropdown title="Reports & Analytics" active={activeKey.split('.')[0] === "7"}>
+                        <NavDropdown.Item eventKey="7.1">Generate Reports</NavDropdown.Item>
+                        <NavDropdown.Item eventKey="7.2">Data Visualization</NavDropdown.Item>
+                    </NavDropdown>
+                    <NavDropdown title="Security & Access Control" active={activeKey.split('.')[0] === "8"}>
+                        <NavDropdown.Item eventKey="8.1">User Authentication</NavDropdown.Item>
+                        <NavDropdown.Item eventKey="8.2">Audit Trails</NavDropdown.Item>
+                    </NavDropdown>
+                    <NavDropdown title="Communication" active={activeKey.split('.')[0] === "9"}>
+                        <NavDropdown.Item eventKey="9.1">Messaging</NavDropdown.Item>
+                        <NavDropdown.Item eventKey="9.2">Notifications</NavDropdown.Item>
+                    </NavDropdown>
+                </Nav>
+            </Navbar>
             {activeKey === "1" && (
-                <Dashboard/>
+                <Dashboard />
             )}
             {activeKey === "2.1" && (
-                <PatientRegistration/>
+                <PatientRegistration />
             )}
             {activeKey === "2.2" && (
                 <PatientRecords />
