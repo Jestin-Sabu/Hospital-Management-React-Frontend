fe7e9aef4d57691becd2d6f1ebe48019a4b8714c
Jestin Sabu <55315353+Jestin-Sabu@users.noreply.github.com>
2023-10-07 23:44:30 +0530

    Redesign Patient Registeration Form

--- hospitalmanagement/src/components/PatientManagement/PatientRegistration.jsx
+++ hospitalmanagement/src/components/PatientManagement/PatientRegistration.jsx
@@ -1,11 +1,11 @@
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Stack from 'react-bootstrap/Stack';
-import Button from 'react-bootstrap/Button';
 import User from '../../models/User';
 import AuthService from '../../services/AuthService';
-import { useState } from 'react';
-import Card from 'react-bootstrap/Card';
 import MessageModal from '../MessageModal';
 
 
@@ -55,31 +55,31 @@ const PatientRegistration = () => {
     };
     return (
         <>
-            <Container className="mt-3">
-                <Card>
-                    <Card.Header className="bg-dark text-white">Patient Registration</Card.Header>
+            <Container className="mt-2" data-bs-theme="dark">
+                <Card className="shade">
+                    <Card.Header as="h4">Patient Registration</Card.Header>
                     <Card.Body>
                         <Form noValidate validated={signUpValidated} onSubmit={handleSignUpSubmit}>
                             <Stack direction="horizontal" gap={2}>
-                                <Form.Control id="inputFirstName" type="text" placeholder="First Name" required className="rounded-pill border-2 shadow px-4 mb-3" />
-                                <Form.Control id="inputLastName" type="text" placeholder="Last Name" required className="rounded-pill border-2 shadow px-4 mb-3" />
+                                <Form.Control id="inputFirstName" type="text" placeholder="First Name" required className="shade rounded-pill border-1 shadow px-4 mb-3 " />
+                                <Form.Control id="inputLastName" type="text" placeholder="Last Name" required className="shade rounded-pill border-1 shadow px-4 mb-3 " />
                             </Stack>
-                            <Form.Control id="inputAddress" as="textarea" placeholder="Address" required className="rounded border-2 shadow px-4 mb-3" style={{ resize: 'None', height: "100px" }} />
+                            <Form.Control id="inputAddress" as="textarea" placeholder="Address" required className=" shade rounded border-1 shadow px-4 mb-3 " style={{ resize: 'None', height: "100px" }} />
                             <Stack direction="horizontal" gap={2}>
-                                <Form.Control id="inputPin" type="text" placeholder="PIN" required className="rounded-pill border-2 shadow px-4 mb-3" />
-                                <Form.Control id="inputMobileNumber" type="text" placeholder="Mobile Number" required className="rounded-pill border-2 shadow px-4 mb-3" />
-                            </Stack>
-                            <Stack direction="horizontal" gap={2}>
-                                <Form.Control id="inputSignUpUserName" type="text" placeholder="Username" required className="rounded-pill border-2 shadow px-4 mb-3" />
-                                <Form.Control id="inputSignUpEmail" type="email" placeholder="Email address" required className="rounded-pill border-2 shadow px-4 mb-3" />
+                                <Form.Control id="inputPin" type="text" placeholder="PIN" required className="shade rounded-pill border-1 shadow px-4 mb-3 " />
+                                <Form.Control id="inputMobileNumber" type="text" placeholder="Mobile Number" required className="shade rounded-pill border-1 shadow px-4 mb-3 " />
                             </Stack>
                             <Stack direction="horizontal" gap={2}>
-                                <Form.Control id="inputSignUpPassword" type="password" placeholder="Password" required className="rounded-pill border-2 shadow px-4 mb-3 text-primary" />
-                                <Form.Control id="inputSignUpConfirmPassword" type="password" placeholder="Confirm Password" required className="rounded-pill border-2 shadow px-4 mb-3 text-primary" />
+                                <Form.Control id="inputSignUpUserName" type="text" placeholder="Username" required className="shade rounded-pill border-1 shadow px-4 mb-3 " />
+                                <Form.Control id="inputSignUpEmail" type="email" placeholder="Email address" required className="shade rounded-pill border-1 shadow px-4 mb-3 " />
+                            </Stack>
+                            <Stack direction="horizontal" gap={2}>
+                                <Form.Control id="inputSignUpPassword" type="password" placeholder="Password" required className="shade rounded-pill border-1 shadow px-4 mb-3  text-primary" />
+                                <Form.Control id="inputSignUpConfirmPassword" type="password" placeholder="Confirm Password" required className="shade rounded-pill border-1 shadow px-4 mb-3  text-primary" />
                             </Stack>
                             <div className="d-flex justify-content-between">
-                                <Button variant="secondary" type="reset">Cancel</Button>
-                                <Button variant="primary" type="submit">Submit</Button>
+                                <Button variant="danger" type="reset">Cancel</Button>
+                                <Button variant="success" type="submit">Submit</Button>
                             </div>
                         </Form>
                     </Card.Body>
